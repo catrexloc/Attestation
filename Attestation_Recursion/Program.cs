fc2ce5b943fb1a8678a77e010f0db76ac473cd4f
Alex Galuza <0.chto.a.cho.0@gmail.com>
2024-02-02 12:54:54 +0300

    Функция Аккермана

--- Attestation_Recursion/Program.cs
+++ Attestation_Recursion/Program.cs
@@ -2,17 +2,44 @@
 // которая выведет все натуральные числа в промежутке от M до N.
 // Использовать рекурсию, не использовать циклы.
 
+// int rec(int m, int n)
+// {
+//     if (m == n)
+//         return n;
+//     else
+//         Console.WriteLine(m);
+//     return m + rec(m + 1, n);
+// }
+// Console.Write("Вывести числа в промежутке");
+// Console.Write("от: ");
+// int m = Convert.ToInt32(Console.ReadLine());
+// Console.Write("до: ");
+// int n = Convert.ToInt32(Console.ReadLine());
+// rec(m, n);
+
+
+// Задача 2: Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n
+
 int rec(int m, int n)
 {
-    if (m == n)
-        return n;
+    if (m == 0)
+    {
+        return n + 1;
+    }
+    else if (n == 0 && m > 0)
+    {
+        return rec(m - 1, 1);
+    }
     else
-        Console.WriteLine(m);
-    return m + rec(m + 1, n);
+    {
+        return (rec(m - 1, rec(m, n - 1)));
+    }
 }
-Console.Write("Вывести числа в промежутке");
-Console.Write("от: ");
+Console.Clear();
+Console.Write("Число m: ");
 int m = Convert.ToInt32(Console.ReadLine());
-Console.Write("до: ");
+
+Console.Write("Число n: ");
 int n = Convert.ToInt32(Console.ReadLine());
-rec(m, n);
+
+Console.Write($"m = {m}, n = {n} => {rec(m, n)}"); 
